90f3a2bf27f82304349ba2a97e7be8bd1a49f2f4
Max Weffers <rcstar6696@gmail.com>
2017-09-20 21:54:35 +0200

    z3c: Add missing permission for core hardware

--- z3c.mk
+++ z3c.mk
@@ -37,6 +37,10 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
 # Dalvik/HWUI
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
