ce906ccf76d91173017556a6b178fed00492e11b
Max Weffers <rcstar6696@gmail.com>
2017-12-30 21:53:18 +0100

    z3c:Test: move audio policy to shinano
    Change-Id: Icdb2e48337ea99b397ea7d847bdbcc5bb0eec895

---
+++ product-common/audio.mk
@@ -0,0 +1,4 @@
+# Audio
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml \
+   $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/vendor/etc/mixer_paths_auxpcm.xml
