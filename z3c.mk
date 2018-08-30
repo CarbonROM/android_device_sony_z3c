4862ac4510582753878d9cc5158e0457e075ce79
jrior001 <jriordan001@gmail.com>
2018-08-29 22:51:19 -0400

    nfc: libnfc-brcm.conf -> libnfc-nci.conf
    Change-Id: Ib234f09778166848d66de6ba0f1d5237d839de0c

--- z3c.mk
+++ z3c.mk
@@ -51,7 +51,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/libnfc-nci.conf:system/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
 
 # Sensors
