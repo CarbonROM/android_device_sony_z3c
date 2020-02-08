d13e8b4267f901554e939e12d09be49cd3316ec4
Christian Oder <myself5@carbonrom.org>
2020-02-08 15:24:19 +0100

    z3c: Carbonify
    Change-Id: I05c5f167531d45f2c506473674e0312be2a4c6e2

--- z3c.mk
+++ z3c.mk
@@ -37,8 +37,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Properties
 include $(LOCAL_PATH)/system_prop.mk
